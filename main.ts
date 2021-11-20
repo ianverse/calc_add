7f0dc95d472f4539c19c09f1522365c9794fdd69
ianverse <93848766+ianverse@users.noreply.github.com>
2021-11-20 11:11:56 +0900

    simplify B event driven function

--- main.ts
+++ main.ts
@@ -4,30 +4,17 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.B, function () {
     if (order == 0) {
-        basic.showLeds(`
-            . . . . .
-            . . # . .
-            . # # # .
-            . . # . .
-            . . . . .
-            `)
+        basic.showString("+")
+        Sum += num
         order += 1
-        Sum = Sum + num
-        num = 0
-    } else if (order == 1) {
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            . . . . .
-            . # # # .
-            . . . . .
-            `)
-        Sum = Sum + num
+    } else {
+        basic.showString("=")
+        Sum += num
         basic.showNumber(Sum)
         order = 0
-        num = 0
         Sum = 0
     }
+    num = 0
 })
 let order = 0
 let num = 0
